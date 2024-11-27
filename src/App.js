820c6ea7debfba585f40e7930b080fc4a1fc1136
NandhiniRavi01 <113678413+NandhiniRavi01@users.noreply.github.com>
2024-11-27 10:09:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";  // Use Routes instead of Switch
 import Customer from "./Component/Customer";
 import CustomerDetails from './Component/CustomerDetails';
+import DeliverPerson from "./Component/DesktopUser/DeliverPerson.js";
+import OrderList from "./Component/DesktopUser/OrderList.js";
+import NotifiactionDesktop from "./Component/DesktopUser/NotifiactionDesktop.js";
+import Settings from "./Component/DesktopUser/Settings.js";
 
 function App() {
   return (
@@ -12,7 +16,12 @@ function App() {
         <Routes> {/* Use Routes instead of Switch */}
           <Route path="/" element={<Customer />} /> {/* Use element prop with JSX */}
           <Route path="/customer-details" element={<CustomerDetails />} />
-          
+{/*Dharun*/}
+            <Route path="/deliveryperson" element={<DeliverPerson/>} />
+        <Route path="/notification" element={<NotifiactionDesktop/>} />
+        <Route path="/settings" element={<Settings/>} />
+        <Route path="/orderlist" element={<OrderList/>} />
+
           {/* Add other routes here */}
         </Routes>
       </div>
